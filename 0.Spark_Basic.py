990c6a841451584d250267b5554ad3f66af39f14
Jaejun <92967394+jaejunjeong@users.noreply.github.com>
2023-05-21 16:39:16 +0900

    Update 0.Spark_Basic.py

--- 0.Spark_Basic.py
+++ 0.Spark_Basic.py
@@ -35,3 +35,78 @@ xrangeRDD = sc.parallelize(data, 4)
 
 # this will let us know that we created an RDD
 xrangeRDD
+
+# subRDD reduce each element in the RDD by 1. filterdRDD then filter the RDD to only contain elements <10.
+subRDD = xrangeRDD.map(lambda x: x-1)
+filteredRDD = subRDD.filter(lambda x : x<10)
+
+'''
+Transformation functions
+map() - apply function to element (eg. rdd.map(x+2) : apply + 2 to all elements)
+filter() - filter the elements
+distinct() - filter the distinct elements 
+union() - union two RDD 
+intersection() - intersection two RDD
+cache() - cache the RDD for recalculation
+'''
+
+'''
+Actions functions
+collect() - return all data in RDD 
+count() - return count of data in RDD
+top(Num) - return top Num of data in RDD
+takeOrdered(num)(Ordering) - return ordered Num of data in RDD
+reduce(func) - The values of the RDD are calculated in parallel. (Based on calculation: func)
+'''
+
+# Transformation returns a result to the driver. Apply the collect() action to get the output from the transformation.
+print(filteredRDD.collect())
+filteredRDD.count()
+
+##How to create an RDD and cache it. 10x speed improvement. Browse to the Spark UI(host:4040) to see the actual computation time. Second calculation took much less time.
+import time 
+test = sc.parallelize(range(1,50000),4)
+test.cache()
+
+t1 = time.time()
+# first count will trigger evaluation of count *and* cache
+count1 = test.count()
+dt1 = time.time() - t1
+print("dt1: ", dt1)
+
+
+t2 = time.time()
+# second count operates on cached data only
+count2 = test.count()
+dt2 = time.time() - t2
+print("dt2: ", dt2)
+
+##Work with the extremely powerful SQL engine in Apache Spark
+# Download the data first into a local `people.json` file
+!curl https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-BD0225EN-SkillsNetwork/labs/data/people.json >> people.json
+
+# Read the dataset into a spark dataframe using the `read.json()` function
+df = spark.read.json("people.json").cache()
+
+# Print the dataframe as well as the data schema
+df.show()
+df.printSchema()
+
+# Register the DataFrame as a SQL temporary view
+df.createTempView("people")
+
+# Select and show basic data columns(DF and Spark)
+df.select("name").show()
+df.select(df["name"]).show()
+spark.sql("SELECT name FROM people").show()
+
+# Perform basic filtering(DF and Spark)
+df.filter(df["age"] > 21).show()
+spark.sql("SELECT age, name FROM people WHERE age > 21").show()
+
+# Perfom basic aggregation of data
+df.groupBy("age").count().show()
+spark.sql("SELECT age, COUNT(age) as count FROM people GROUP BY age").show()
+
+
+
